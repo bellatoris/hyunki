8fa8fc250ddfab56f82ee1d705723a6210203099
bellatoris <bellatoris@snu.ac.kr>
2017-12-15 00:25:01 +0900

    html structure complete

--- src/components/home.js
+++ src/components/home.js
@@ -12,27 +12,43 @@ class Home extends Component {
         <section id="home">
           <div className="overlay"></div>
           <div className="home-content">
-            <h3>
-              Hello There
-          </h3>
-            <h1>
-              I am Hyunki Min.
-              I am a PhD Student, School of Mathematics in
-              Georgia Institute of Technology
-          </h1>
-            <div className="home-content_buttons btn">
-              <a href="#works" className="smoothscoll">
-                Preprints
-            </a>
-              <a href="#about" className="smoothscoll">
-                More About Me
-            </a>
+            <div className="home-content__main">
+              <h3>
+                Hello There
+              </h3>
+              <h1>
+                I am Hyunki Min.
+                I am a PhD Student, School of Mathematics in
+                Georgia Institute of Technology
+              </h1>
+              <div className="home-content__buttons">
+                <a href="#preprints" className="smoothscoll btn btn--stroke">
+                  Preprints
+                </a>
+                <a href="#about" className="smoothscoll btn btn--stroke">
+                  More About Me
+                </a>
+              </div>
+              <div className="home-content__scroll">
+                <a href="#about" class="scroll-link smoothscroll">
+                  <span>Scroll Down</span>
+                </a>
+              </div>
             </div>
-            <div className="home-content-scroll">
-              <a href="#about" class="scroll-link smoothscroll">
-                <span>Scroll Down</span>
-              </a>
-            </div>
+            <ul className="home-social">
+              <li>
+                <a href="#0">
+                  <i className="im im-facebook"></i>
+                  <span>Facebook</span>
+                </a>
+              </li>
+              <li>
+                <a href="#0">
+                  <i className="im im-twitter"></i>
+                  <span>Twitter</span>
+                </a>
+              </li>
+            </ul>
           </div>
         </section>
       </div>
--- src/components/navigator.js
+++ src/components/navigator.js
@@ -8,14 +8,31 @@ class Navigator extends Component {
 
   render() {
     return (
-      <div className="App-navigator">
-        <ul>
-          <li><a href="#Profile">Profile</a></li>
-          <li><a href="#Research">Research</a></li>
-          <li><a href="#Preprints">Preprints</a></li>
-          <li><a href="#Contacts">Contacts</a></li>
-        </ul>
-      </div>
+      <header className="s-header">
+        <div className="header-logo">
+          <a className="site-logo" href="index.html">
+            <h1>Hyunki</h1>
+          </a>
+        </div>
+        <nav className="header-nav-wrap">
+          <ul className="header-nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home" title="home">Home</a>
+            </li>
+            <li className="">
+              <a className="smoothscroll" href="#about" title="about">About</a>
+            </li>
+            <li className="">
+              <a className="smoothscroll" href="#preprints" title="preprints">Preprints</a>
+            </li>
+            <li className="">
+              <a className="smoothscroll" href="#contact" titls="contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <a className="header-menu-toggle" href="#0">
+        </a>
+      </header>
     );
   }
 }
